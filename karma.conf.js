73ae848adc8f8214e150fa574a39150a996291d3
marrero- <alexmd54@gmail.com>
2016-04-04 18:26:20 +0100

    Se han añadido los ficheros de gulp, karma, travis y algunos de los necesarios para las pruebas

---
+++ karma.conf.js
@@ -0,0 +1,48 @@
+// Karma configuration
+// Generated on Fri Mar 31 2016 21:30:24 GMT+0000 (WET)
+
+module.exports = function(config) {
+  config.set({
+  
+  basePath: '',
+  
+  frameworks: ['mocha', 'chai'],
+  
+   files: [
+     'test/index.html',
+      'assets/js/*',
+      'test/*.js',
+      'test/*.css'
+    ],
+    
+  client: {
+          mocha: {
+            ui: 'bdd'
+          }
+    },
+    
+  exclude: [
+      'gulpfile.js'
+    ],
+    
+  preprocessors: {
+      'test/index.html': ['html2js']
+    },
+    
+  reporters: ['progress'],
+
+  port: 9876,
+
+  colors: true,
+
+  logLevel: config.LOG_INFO,
+
+  autoWatch: true,
+
+     browsers: ['Firefox', 'PhantomJS'], 
+
+  singleRun: false
+  
+  
+    });
+};

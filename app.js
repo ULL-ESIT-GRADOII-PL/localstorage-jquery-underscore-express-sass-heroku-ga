44cbb5d4f617115132af2a046248411ed6856e6e
Gabriel Melian Hernandez <alu0100819786@ull.edu.es>
2016-04-06 22:16:17 +0100

    Añadido Script para Express y Heroku #9

---
+++ app.js
@@ -0,0 +1,19 @@
+//Server para Heroku
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', function (req, res) {
+  red.render('pages/index');
+  });
+  
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+  });

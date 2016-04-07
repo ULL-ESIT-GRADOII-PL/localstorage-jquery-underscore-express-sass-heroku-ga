5d2a3c53f2998400eaf4a483b143c70f74ace018
marrero- <alexmd54@gmail.com>
2016-04-07 20:45:55 +0100

    #9 Se ha arreglado la ruta desde la cual el servidor sirve los ficheros

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/minified'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
 app.get('/', function (req, res) {
-  res.render('pages/index');
+  res.render('minified/index');
   });
-  
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
   });
